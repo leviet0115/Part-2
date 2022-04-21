aff65233b37a85aa9a76990a4a186f8ce6950c0d
Le Viet <le.viet@MacBook-Pro-2.local>
2022-04-21 15:38:34 +0300

    exercise 2.5

--- course/src/App.js
+++ course/src/App.js
@@ -1,27 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((total, part) => total + part.exercises, 0);
-  return <p>Total of {total} exercises </p>
-};
-
-const Part = ({ part }) => (
-  <p>{part.name} {part.exercises}</p>
-);
-
-const Content = ({ parts }) => (
-  parts.map(part => <Part part={part} key={part.id}/>)
-);
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name}></Header>
-      <Content parts={course.parts}></Content>
-      <Total parts={course.parts}></Total>
-    </>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
